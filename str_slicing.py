e61f64d34efa28f713c93d5bfd5020298bc6332f
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2022-07-01 16:19:31 +0530

    Add files via upload

---
+++ str_slicing.py
@@ -0,0 +1,34 @@
+p='python'
+print(p[:])
+print(p[0:])
+print(p[1:])
+print(p[2:])
+print(p[4:])
+print(p[5:])
+print(p[:2])
+print(p[:4])
+print(p[:5])
+print(p[0:4])
+print(p[2:4])  #th
+print(p[4:2])  #Blankline
+print(p[2:2])  #Blankline
+print(p[3:4])  #h
+print(p[5:2])  #Blankline
+            
+p='pythonWorld'  ; a='123486445'
+
+print(p[6:11])
+print(p[0:6])
+print(p[2:3])
+print(p[0], p[-1])
+print(p[0:-2])
+print(p[5],p[3])
+print(p[5],p[3])
+print(p[5],p[-5])
+print(p[-5],p[-1])
+print(p[2:6])
+print(p[-5],p[-6],p[4])
+print(p[2:5]+p[6:9])
+print(p[0:6]+str(a[0:-1]))
+
+
