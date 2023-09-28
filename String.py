cc08714e6c1bc5f2aa89118ccceb2f2b97ffbecf
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2023-09-28 17:38:07 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,46 @@
+#name = input("Enter your name")
+#age = input("Enter age")
+#print("My name is", name, "and age is", age)
+#para = '''Hi guys...This is Pranita this side
+#and wanted to inform you that
+#todays luch offered to all of you by me'''
+#print(para)
+#print(para[5:-5])
+#print(para[55:-1])
+
+#name = "Pranita"
+#friend = "Vaibhav"
+#another_friend = "shiv"
+#apple = '''He said 
+#Hi Pranita 
+#Hey im good
+#"I want to eat an apple" '''
+#print(apple)           
+#print("Hello",name)
+#print(name[0])
+#print(name[1])
+#print(name[2])
+#print(name[3])
+#print(name[4])
+#print(name[5])
+#print(name[6])
+#print("Lets use a for loop \n")
+#for character in name:
+#    print(character)
+#print(len(name))
+
+pie = "Applepie"
+print(pie[:5])
+print(pie[6])
+print(pie[2:-2])
+print(pie[6:])
+print(pie[:6])
+print(pie[2:6])
+print(pie[-8:])
+print(pie[-1:-3])
+for i in pie:
+    #print(i+i)
+    print(i, i+i)
+print(pie[-4:-2])    
+    
+     
